65772f6ec891116fecb5bfefc712e51a783c925c
unknown <jdoujet@gmail.com>
2023-09-24 18:19:55 -0400

    update typescript

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-import { KameleoonUtils } from 'node_modules/@kameleoon/javascript-sdk';
-
-// -- Constants values
-export const siteCode = "44r9jgmatl";
-export const featureKey = "demo_web_feature_flag";
-export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { KameleoonClient } from 'node_modules/@kameleoon/javascript-sdk';
-import { featureKey, siteCode, visitorCode } from "./constants.js";
-
-console.log('test')
-
-const client = new KameleoonClient(siteCode);
