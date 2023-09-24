a0b341dc7c64769a2a925fe9e3f075acd5a57302
unknown <jdoujet@gmail.com>
2023-09-24 17:39:50 -0400

    call kameleoonClient

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { KameleoonClient } from 'node_modules/@kameleoon/javascript-sdk';
+import { KameleoonClient } from '../node_modules/@kameleoon/javascript-sdk';
 import { featureKey, siteCode, visitorCode } from "./constants.js";
 
 console.log('test')
+
+const client = new KameleoonClient(siteCode);
