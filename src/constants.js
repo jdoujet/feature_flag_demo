218c608f5b5cfbcf7deb6f4eeb1cf9e10df1fb00
unknown <jdoujet@gmail.com>
2023-09-24 18:53:48 -0400

    update package

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.visitorCode = exports.featureKey = exports.siteCode = void 0;
-const javascript_sdk_1 = require("@kameleoon/javascript-sdk");
-// -- Constants values
-exports.siteCode = "44r9jgmatl";
-exports.featureKey = "demo_web_feature_flag";
-exports.visitorCode = javascript_sdk_1.KameleoonUtils.getVisitorCode("www.example.com");
--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const javascript_sdk_1 = require("@kameleoon/javascript-sdk");
-const constants_1 = require("./constants");
-console.log('test');
-const client = new javascript_sdk_1.KameleoonClient(constants_1.siteCode);
