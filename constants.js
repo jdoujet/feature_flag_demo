c89934638bfa9015e0f53e5f7789ee10f9546e0f
unknown <jdoujet@gmail.com>
2023-09-23 23:35:00 -0400

    init project

---
+++ constants.js
@@ -0,0 +1,7 @@
+/*import { KameleoonUtils } from "@kameleoon/javascript-sdk";
+
+// -- Constants values
+export const siteCode = "dhqz9s1tb6";
+export const featureKey = "bouncing_ball_demo";
+export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
+*/
